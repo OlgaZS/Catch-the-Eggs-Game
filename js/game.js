a522eae8cb3c45de7e87e367b8802e3b9eb54a46
Olga Zinoveva <ozinka81@gmail.com>
2019-07-03 22:39:42 +0200

    create function to draw  canvas

--- js/game.js
+++ js/game.js
@@ -4,7 +4,26 @@ class Game {
     this.player = new Player();
   }
 
+  pause() {
+    if (this.intervalGame) {
+      window.cancelAnimationFrame(this.intervalGame);
+      this.intervalGame = undefined;
+    }
+  }
+
   start() {
     this.player.draw(this.ctx);
+  
+    pintarCanvas() {
+    this.player.x++;
+    requestAnimationFrame(pintarCanvas)}
+  
   }
+  pintarCanvas();
+
+ let requestAnimationFrame = window.requestAnimationFrame(start);
+
+  // window.requestAnimationFrame(){
+  // this.player.x++;
+  // }
 }
