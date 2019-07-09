aa614508758d4b4881767a5631b4915c9baa38d0
Olga Zinoveva <ozinka81@gmail.com>
2019-07-09 21:04:26 +0200

    add collission function

--- js/game.js
+++ js/game.js
@@ -1,8 +1,9 @@
 class Game {
   constructor(ctx) {
     this.ctx = ctx;
+    this.points = 0;
     this.player = new Player();
-    this.egg = new Egg("goodEgg", 10, 5, 70, 20);
+    this.egg = new Egg("goodEgg", 1, 10, 200, 20);
   }
 
   start() {
@@ -36,20 +37,20 @@ class Game {
     document.onkeydown = e => {
       switch (e.keyCode) {
         case 38: // arrow up
-          console.log("key up");
+          // console.log("key up");
           break;
         case 40: // arror down
-          console.log("key down");
+          // console.log("key down");
           break;
         case 37:
           this.player.moveLeft(); // arror left
-          console.log("key left");
-          console.log(this.player.position.x);
+          // console.log("key left");
+          // console.log(this.player.position.x);
           break;
         case 39:
           this.player.moveRight(); // arrow right
-          console.log("key right");
-          console.log(this.player.position.x);
+          // console.log("key right");
+          // console.log(this.player.position.x);
           break;
         case 80: // p pause
           break;
@@ -59,10 +60,30 @@ class Game {
 
   update() {
     this.ctx.clearRect(0, 0, 800, 600);
-    console.log("update");
     this._drawPlayer();
     this._drawEgg();
+    this.collission();
     this._checkEggHitBottom();
     window.requestAnimationFrame(this.update.bind(this));
+  }
+
+  collission() {
+    const eggBottonLeftX = this.egg.x + this.egg.height;
+    const playerTopLeftX = this.player.position.x;
+    const playerTopRightX = this.player.position.x + this.player.width;
+    const eggBottonRightX = this.egg.x + this.egg.width;
+    const eggBottonY = this.egg.y;
+    const playerTop = this.player.position.y;
+    if (
+      ((eggBottonLeftX >= playerTopLeftX &&
+        eggBottonLeftX <= playerTopRightX) ||
+        (eggBottonRightX >= playerTopLeftX &&
+          eggBottonRightX <= playerTopRightX)) &&
+      eggBottonY >= playerTop
+    ) {
+      console.log("Collission");
+      this.points += this.egg.points;
+      console.log(`Points: ${this.points}`);
+    }
   }
 }
